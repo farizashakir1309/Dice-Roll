812e21ab4f046c2dc25c5e1e492515c034c4a29b
unknown <farizashakir13@gmail.com>
2020-11-14 17:08:00 +0530

    add RollDice component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Die from './Die.js';
+import RollDice from './RollDice.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <RollDice/>
+    </div>
+    
+  );
+}
+
+export default App;
